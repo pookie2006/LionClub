022dad2e84017e3f91fba188d313796e40774812
t-ffz <toffz1110@gmail.com>
2025-11-24 17:37:25 -0500

    preparing for deploying

--- backend/server.js
+++ backend/server.js
@@ -15,4 +15,4 @@ const clubs = JSON.parse(fs.readFileSync(path.join(__dirname, 'clubs.json'), 'ut
 // Return clubs JSON
 app.get('/clubs', (req, res) => res.json(clubs));
 
-app.listen(3000, () => console.log("Backend running on http://localhost:3000"));
+app.listen(3000, () => console.log("Backend running on https://lionclub.onrender.com"));
